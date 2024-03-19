36b06e2c5f23e57fc4f706bd437d663552c95bdc
LONG Kongheng <kongheng.long@sbilhbank.com.kh>
2024-03-19 10:25:48 +0700

    inital project

---
+++ src/main/java/kh/com/sbilhbank/oauth2/SpringBootWithKeyCloakApplication.java
@@ -0,0 +1,13 @@
+package kh.com.sbilhbank.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithKeyCloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithKeyCloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kh/com/sbilhbank/oauth2/SpringBootWithKeyCloakApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.com.sbilhbank.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithKeyCloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
